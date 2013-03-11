f293edbc5727dcac7c27ae685a903c87dc40c68b
Brooks <brooks.kindle@wsu.edu>
2013-03-10 20:36:13 -0700

    UPDATE: pyGoogleText.py
    quantifyTime, sortMostRecent, and compare functions created. Not tested.

--- pyGoogleText.py
+++ pyGoogleText.py
@@ -4,10 +4,22 @@ import time
 def quantifyTime(msg):
     """returns how old the text message is
     msg is the message
-    returns an integer value representing
-    the age of the text message (starting
-    from Jan 01, 2000)"""
-    pass
+    returns age
+    where age is an integer value representing
+    the age of the text message in seconds
+    (since Jan 01, 2000)"""
+    mo = [31, 28, 31, 30, 31, 30,
+          31, 31, 30, 31, 30, 31]
+    age = 0 #age of text
+    t = msg.get("time")
+    age += (t.years - 2000) * 365 * 24 * 60 * 60
+    for i in range(t.months):
+        age += mo[i] * 24 * 60 * 60
+    age += t.day * 24 * 60 * 60
+    age += t.hour * 60 * 60
+    age += t.minute * 60
+    age += t.second
+    return age
                 
 def getLogin():
     """prompts user for gvoice login info
@@ -24,8 +36,23 @@ def getPhone():
 
 def sortMostRecent(msglist):
     """sorts message list from newest to oldest"""
-    pass
+    msglist.sort(compare)
 
+def compare(msg1, msg2):
+    """compares two text messages by date sent
+    msg1 is the first message
+    msg2 is the second message
+    returns 1 if msg1 is older than msg2
+    returns -1 if msg1 is newer than msg2
+    returns 0 if msg1 and msg2 have same age"""
+    age1 = quantifyTime(msg1)
+    age2 = quantifyTime(msg2)
+    if(age1 > age2): #msg1 is older
+        return 1
+    if(age1 < age2): #msg1 is newer
+        return -1
+    return 0 #same age
+    
 def getUnreadTexts(gvoice):
     """returns a sorted message list of unread texts
     gvoice is the Voice() instance
